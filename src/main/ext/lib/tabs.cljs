e99bdc83bdc3dc0b384334f8081f34bb1082ae76
Florian Schroedl <flo.schroedl@gmail.com>
2025-05-05 09:50:04 +0200

    Only close other tab items of the current tab group

--- src/main/ext/lib/tabs.cljs
+++ src/main/ext/lib/tabs.cljs
@@ -4,10 +4,13 @@
    [ext.lib.chrome :as chrome]))
 
 (defn close-non-selected! []
-  (p/let [tabs (chrome/current-window-tabs+ {:active false
-                                             :pinned false})]
-    (doseq [t tabs]
-      (js/chrome.tabs.remove (.-id t)))))
+  (p/let [[active-tab] (chrome/current-window-tabs+ {:active true})
+          inactive-tabs (chrome/current-window-tabs+ {:active false
+                                                      :pinned false})
+          active-group-id (.-groupId active-tab)]
+    (doseq [tab inactive-tabs
+            :when (= (.-groupId tab) active-group-id)]
+      (js/chrome.tabs.remove (.-id tab)))))
 
 (defn pin-selected! []
   (p/let [[tab] (chrome/current-window-tabs+ {:active true})]
